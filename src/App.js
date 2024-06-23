ef8f216cf473091087b407b76aeab8d184243d43
Anand Raut <Anand.Raut@ril.com>
2024-06-23 16:48:41 +0530

    FEATURE: Add Download zip feature

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 
   const getFile = async () => {
     try {
-      const data = await fetch("http://localhost:3200/download-zip")
+      fetch("http://localhost:3200/download-zip")
         .then((response) => {
           if (!response.ok) {
             throw new Error("Network response was not ok");
